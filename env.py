e8f3a1361d4a6d5f227328934dc27b7f0bde9701
Tes R <tesryu39@gmail.com>
2024-01-28 13:37:03 +0000

    Update database url

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "<postgres://ushk8vximwg:sJ2doMew4wXq@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/mango_stick_tacky_881154>")
+    "DATABASE_URL", "postgres://ushk8vximwg:sJ2doMew4wXq@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/mango_stick_tacky_881154")
