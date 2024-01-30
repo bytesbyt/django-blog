b6c1ab62a72b8ccbbb283763e9004cfbe5c63277
Tes R <tesryu39@gmail.com>
2024-01-30 13:24:44 +0000

    In the post model, add an attributr status defined as an integer field with a default of 0.

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['my-blog01-7dca62971b22.herokuapp.com','.herokuapp.com', 'localhost']
 
