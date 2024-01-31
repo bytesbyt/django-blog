1c19b38282a01607d6fa82f5654a2d6b3d78a541
Tes R <tesryu39@gmail.com>
2024-01-31 21:37:39 +0000

    Update setting: Add -'8000-bytesbyt-djangoblog-hxz3e3uwyrq.ws-eu107.gitpod.io'to allowed host.

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-blog01-7dca62971b22.herokuapp.com','.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['my-blog01-7dca62971b22.herokuapp.com','.herokuapp.com', 'localhost', '8000-bytesbyt-djangoblog-hxz3e3uwyrq.ws-eu107.gitpod.io']
 
 
 # Application definition
