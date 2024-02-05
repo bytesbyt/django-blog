ceb45da0b2167d15f30c680dd5534fb787df7a12
Tes R <tesryu39@gmail.com>
2024-02-05 17:10:12 +0000

    create a variable comment_form and assign the class CommentForm() to it. Add this comment_form variable to the context in the render helper function.

--- blog/views.py
+++ blog/views.py
@@ -30,6 +30,7 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
 
     return render(
         request,
@@ -38,5 +39,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
