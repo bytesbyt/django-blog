370eab8cc34d18e73d74c5fdfdb074532e1b5ab6
Tes R <tesryu39@gmail.com>
2024-02-06 18:10:12 +0000

    Add command to set the vbaluse of the cloudinary_url constant

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://ushk8vximwg:sJ2doMew4wXq@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/mango_stick_tacky_881154")
 os.environ.setdefault("SECRET_KEY", "bNImCiPO")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://588487173924593:XRGRxU9uRacYiQOOSPV93qydzis@da5qiddrg")
